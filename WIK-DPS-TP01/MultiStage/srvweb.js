862dbdd82b9355040150f5bf4c0f26eb5aec3ae1
JeanM Jaws <JeanM.jaws19@outlook.fr>
2026-06-04 15:51:47 +0200

    fix .js + dockerfile

--- WIK-DPS-TP01/MultiStage/srvweb.js
+++ WIK-DPS-TP01/MultiStage/srvweb.js
@@ -2,7 +2,7 @@ import {createServer} from 'http';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const port = process.env.PING_LISTEN_PORT || 8080;
+const port = process.env.PING_LISTEN_PORT || 3030;
 const server = createServer((Request, Response) => {
     if(Request.method === 'GET' && Request.url === '/ping') {
         Response.writeHead(200, {'Content-Type': 'application/json'});
